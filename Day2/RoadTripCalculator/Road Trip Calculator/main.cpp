51cf7e10ba3304dd01bd192cf73aae1fb6cfea54
Brian Erichsen Fagundes <brianerichsen@gmail.com>
2023-08-21 15:34:23 -0600

    adding Road Trip Calculator to Day2

---
+++ Day2/RoadTripCalculator/Road Trip Calculator/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  Road Trip Calculator
+//
+//  Created by Brian Erichsen Fagundes on 8/21/23.
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int distanceInMiles, milesPerGallon;
+    double dollarsPerGallon;
+    
+    cout << "Enter distance traveled in miles: ";
+    cin >> distanceInMiles;
+    
+    cout << "Enter the vehicle's miles per gallon (MPG) efficiency: ";
+    cin >> milesPerGallon;
+    
+    cout << "Enter the cost of gas in dollars per gallon: ";
+    cin >> dollarsPerGallon;
+    
+    double gallonsPerTrip = distanceInMiles / static_cast<double>(milesPerGallon);
+    double costOfTrip = gallonsPerTrip * dollarsPerGallon;
+    
+    cout << "The total cost of the trip in dollars is $ " << fixed << setprecision(2) << costOfTrip << endl;
+    
+    return 0;
+}
