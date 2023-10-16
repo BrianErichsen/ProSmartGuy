27645e0f16ccc1e2bf79fab0addd54730062d9fe
Brian Erichsen Fagundes <brianerichsen@gmail.com>
2023-10-16 17:54:42 -0600

    Adding JavaScript lab

--- CS6011/Week3/Day11/HelloJS/helloJS.js
+++ CS6011/Week3/Day11/HelloJS/helloJS.js
@@ -1,3 +1,4 @@
+function mainFunc() {
 document.writeln("Hello World");
 document.onplaying("Hello World");
 //the difference between the 2 is that writeln writes
@@ -5,3 +6,27 @@ document.onplaying("Hello World");
 //console
 
 let myArray = ["Hello", false, 41, 3.15, {key : "value"}];
+for (let i of myArray) {
+    console.log("elements: ", i);
+}
+myArray[0] = "Oi em portugues";
+myArray.push("69420");
+
+for (let i of myArray) {
+    console.log("elements: ", i);
+}
+console.log(myArray);
+//c++ syntax
+function add(a, b) {
+    return a + b;
+}
+//Literal syntax
+let myFunction = function(a, b) {
+    return a + b;
+}
+console.log(add(3, 5));
+console.log(myFunction(6, 9));
+console.log(myFunction("hi", 5)); //here the output concatenates having hi5 as 
+//result;
+
+}//end of main braket
