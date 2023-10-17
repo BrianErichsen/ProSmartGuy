7bda976d6b88121242b85d1a9b0a0a5d960d4b0c
Brian Erichsen Fagundes <brianerichsen@gmail.com>
2023-10-16 18:10:02 -0600

    adding lab 2 for todays assignment

---
+++ CS6011/Week3/Day11/DomPractice/myPageBuilder.js
@@ -0,0 +1,15 @@
+const h3Element = documet.createElement("h3");
+h3Element.textContent = "Test 69";
+document.body.appendChild(h3Element);
+
+const divElement = document.createElement("div");
+divElement.textContent = "420";
+divElement.style.backgoundColor = "pink";
+divElement.style.padding = "20px";
+document.body.appendChild;
+
+const h1Element = document.querySelector("h1");
+h1Element.style.color = "red";
+
+const h2Element = document.querySelector("h2");
+h2Element.style.fontStyle = "italic";
