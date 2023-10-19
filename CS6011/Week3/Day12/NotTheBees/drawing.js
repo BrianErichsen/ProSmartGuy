dc481743d3bc36d67912f59c725295db2e57325c
Brian Erichsen Fagundes <brianerichsen@gmail.com>
2023-10-18 23:52:37 -0600

    Adding update to hw6

--- CS6011/Week3/Day12/NotTheBees/drawing.js
+++ CS6011/Week3/Day12/NotTheBees/drawing.js
@@ -8,6 +8,17 @@ let context = canvas.getContext("2d");
 let cWidth = canvas.width;
 let cHeight = canvas.height;
 
+//Creates a new image to be used in the background of the game
+let backgroundImage = new Image();
+backgroundImage.src = "images/background.jpeg";
+
+let animationFrameId;
+let gameIsRunning = true;
+
+//Wait for the background image to load
+backgroundImage.onload = function() {
+    //Draws at 0, 0 to cover whole background
+    context.drawImage(backgroundImage, 0, 0);
 //Creates a new object for the player's bee
 let queenBee=new Image();
 queenBee.src = "images/bee.png";
@@ -45,29 +56,83 @@ function animateChasingBees() {
 
         //Sets the speed for the chasing bees
         let speed = 2;
+
+        //Introduces some randomness to the bee's movement
+        //random angle between 0 and 2 * PI
+        let randomAngle = Math.random() * Math.PI * 2;
+        dx += Math.cos(randomAngle) * 20;
+        dy += Math.sin(randomAngle) * 20;
+
         //Updates the speed for the chasing bees
         /*dx/dy over distance represents unit vector pointing from the chasing
         bee to the player's bee and speed asssures that it moves at const speed */
         bee.xPos += (dx / distance) * speed;
         bee.yPos += (dy / distance) * speed;
 
+        // Ensure minimum separation between bees
+        for (let otherBee of bees) {
+            //returns true if both the data type and value are not equal
+            if (otherBee !== bee) {
+                let separation = 50;
+                //Using c^2 = a^2 + b^2;; solving for c
+                let distanceBetweenBees = Math.sqrt(
+                (otherBee.xPos - bee.xPos) ** 2 + (otherBee.yPos - bee.yPos) ** 2
+                );
+                if (distanceBetweenBees < separation) {
+                // Repel the bee from the other bee
+                bee.xPos += (bee.xPos - otherBee.xPos) / distanceBetweenBees * separation;
+                bee.yPos += (bee.yPos - otherBee.yPos) / distanceBetweenBees * separation;
+                }
+            }
+        }
         //Draws the chasing bees at its updated position
         context.drawImage(bee, bee.xPos, bee.yPos);
     }
 }
 function animateImg() {
+    //If gameIsRunnin is not true, then exits the animate loop
+    if (!gameIsRunning) {
+        return;
+    }
     //Clears the canvas
     eraseOld();
+    //Checks for collisions
+    checkCollisions();
     //Draws the player's bees
     context.drawImage(queenBee, queenBee.xPos, queenBee.yPos);
     //Updates and draws the chasing bees
     animateChasingBees();
+    animationFrameId = requestAnimationFrame(animateImg);
     //Resquests the next animation frame
-    window.requestAnimationFrame(animateImg);
+    // window.requestAnimationFrame(animateImg);
 }
+//Function to check for collisions
+    function checkCollisions() {
+        for (let bee of bees) {
+            let dx = queenBee.xPos - bee.xPos;
+            let dy = queenBee.yPos - bee.yPos;
+            let distance = Math.sqrt(dx * dx + dy * dy);
+
+            let collisionThreshold = queenBee.width / 2 + bee.width / 2;
+
+            if (distance < collisionThreshold) {
+                stopGame();
+                return;
+            }
+        }
+    }
+    function stopGame() {
+        gameIsRunning = false;
+        cancelAnimationFrame(animationFrameId);
+
+        context.fillStyle = "black";
+        context.font = "36px Arial";
+        context.fillText("You lost the game", cWidth / 2 - 120, cHeight / 2);
+    }
 function mainDrawing(){
     //Starts the animation loop
-    window.requestAnimationFrame(animateImg);
+    animationFrameId = requestAnimationFrame(animateImg);
+    // window.requestAnimationFrame(animateImg);
 }
 function handleClick(e) {//e represents mouse event
 //updates the xpos and ypos of the image based on the mouse's position
@@ -79,8 +144,11 @@ queenBee.yPos = e.clientY - canvas.getBoundingClientRect().top;
 function eraseOld(){
     //Clears the canvas
     context.clearRect (0,0,cWidth,cHeight);
+    //Redraws background image
+    context.drawImage(backgroundImage, 0, 0);
 }
 // Starts the main drawing loop when the page loads
 window.onload = mainDrawing;
 //Tracks the mouse's position within the canvas
 document.onmousemove = handleClick;
+};
--- CS6011/Week3/Day12/Sorting/sorting.js
+++ CS6011/Week3/Day12/Sorting/sorting.js
@@ -1,20 +1,22 @@
 // Selection Sort
 function FirstSelectionSort(array) {
     for (let i = 0; i < array.length - 1; i++) {
-        //We initialize a variable minIndex to i. This variable will keep track of the index of the minimum element found during each pass of the array.
+        //We initialize a variable minIndex to i. This variable will keep track
+        // of the index of the minimum element found during each pass of the array.
         let minIndex = i;
-        //We start another loop nested inside the outer loop. This inner loop iterates through the array, starting from the element just after the current i
+        //We start another loop nested inside the outer loop. This inner loop iterates
+        //through the array, starting from the element just after the current i
         for (let j = i + 1; j < array.length; j++) {
             if (array[j] < array[minIndex]) {
                 minIndex = j;
             }
         }
-        /*After the inner loop completes, we check if minIndex has changed from 
-        its initial value i. If it has changed, it means we've found a smaller 
+        /*After the inner loop completes, we check if minIndex has changed from
+        its initial value i. If it has changed, it means we've found a smaller
         element than the one at index i.*/
         if (minIndex !== i) {
-            /*minIndex has changed, we swap the element at index i with the minimum 
-            element found at minIndex. This moves the minimum element to 
+            /*minIndex has changed, we swap the element at index i with the minimum
+            element found at minIndex. This moves the minimum element to
             its correct position in the sorted portion of the array.*/
             [array[i], array[minIndex]] = [array[minIndex], array[i]];
         }
@@ -43,42 +45,45 @@ console.log("Sorted floats:", floats);
 FirstSelectionSort(mixedData);
 console.log("Sorted mixed data:", mixedData);
 
-
-
-
 // =====================================  Second Part =====================================//
-//The selectionSort function takes an array to be sorted 
+
+//The selectionSort function takes an array to be sorted
 //and a compareFunction that defines the comparison logic between elements in the array.
 function selectionSort(array, compareFunction) {
-    //We start a loop that iterates through the array, from the first element (index 0) to the second-to-last element. The reason we stop at the second-to-last element is that the last element will already be in its correct position when all others are sorted.
+    //We start a loop that iterates through the array, from the first element
+    //(index 0) to the second-to-last element. The reason we stop at the
+    //second-to-last element is that the last element will already be in its
+    //correct position when all others are sorted.
     for (let i = 0; i < array.length - 1; i++) {
-        //We initialize a variable minIndex to i. This variable will keep track of the index of the minimum element found during each pass of the array.
+        //We initialize a variable minIndex to i. This variable will keep track
+        // of the index of the minimum element found during each pass of the array.
         let minIndex = i;
-        //We start another loop nested inside the outer loop. This inner loop iterates through the array, starting from the element just after the current i
+        //We start another loop nested inside the outer loop. This inner loop
+        //iterates through the array, starting from the element just after the current i
         for (let j = i + 1; j < array.length; j++) {
-            /*Within the inner loop, we use the compareFunction to compare the 
-            element at index j with the element at minIndex. If the result of the 
-            comparison is less than 0, it means that the element at index j is 
-            smaller (according to the comparison logic) than the current minimum 
+            /*Within the inner loop, we use the compareFunction to compare the
+            element at index j with the element at minIndex. If the result of the
+            comparison is less than 0, it means that the element at index j is
+            smaller (according to the comparison logic) than the current minimum
             element at minIndex.*/
             if (compareFunction(array[j], array[minIndex]) < 0) {
-                /*If we find a smaller element at index j, we update minIndex to 
-                be j, indicating that we've found a new minimum element in the 
+                /*If we find a smaller element at index j, we update minIndex to
+                be j, indicating that we've found a new minimum element in the
                 remaining unsorted portion of the array.*/
                 minIndex = j;
             }
         }
-        /*After the inner loop completes, we check if minIndex has changed from 
-        its initial value i. If it has changed, it means we've found a smaller 
+        /*After the inner loop completes, we check if minIndex has changed from
+        its initial value i. If it has changed, it means we've found a smaller
         element than the one at index i.*/
         if (minIndex !== i) {
-            /*minIndex has changed, we swap the element at index i with the minimum 
-            element found at minIndex. This moves the minimum element to 
+            /*minIndex has changed, we swap the element at index i with the minimum
+            element found at minIndex. This moves the minimum element to
             its correct position in the sorted portion of the array.*/
             [array[i], array[minIndex]] = [array[minIndex], array[i]];
 
         }
-        /*The outer loop continues, and the process is repeated for the next 
+        /*The outer loop continues, and the process is repeated for the next
         unsorted element until the entire array is sorted.*/
     }
 }
@@ -86,8 +91,8 @@ function selectionSort(array, compareFunction) {
 
 // =======================   Helper function to find minimum location   ========================//
 
-/*The findMinLocation function is a helper function used in the selection sort 
-algorithm to find the index of the minimum element within a specific portion of 
+/*The findMinLocation function is a helper function used in the selection sort
+algorithm to find the index of the minimum element within a specific portion of
 an array.*/
 
 /*The findMinLocation function takes three parameters:
@@ -95,19 +100,20 @@ array: The array in which we want to find the minimum element's index.
 startIndex: The index from which to start the search for the minimum element.
 compareFunction: A function that defines the comparison logic to determine the order of elements.*/
 function findMinLocation(array, startIndex, compareFunction) {
-    //We initialize a variable minIndex to startIndex. 
+    //We initialize a variable minIndex to startIndex.
     //This variable will keep track of the index of the minimum element found during the search.
     let minIndex = startIndex;
-    /*We start a loop that iterates through the array, beginning from the element 
+    /*We start a loop that iterates through the array, beginning from the element
     just after the startIndex*/
     for (let i = startIndex + 1; i < array.length; i++) {
-        /*Within the loop, we use the compareFunction to compare the element at index i with 
-        the element at the current minIndex. If the result of the comparison is less than 0, 
-        it means that the element at index i is smaller (according to the comparison logic) 
+        /*Within the loop, we use the compareFunction to compare the element at index i with
+        the element at the current minIndex. If the result of the comparison is less than 0
+        it means that the element at index i is smaller (according to the comparison logic)
         than the element at the current minIndex.*/
         if (compareFunction(array[i], array[minIndex]) < 0) {
-            /*If we find a smaller element at index i, we update minIndex to be i, 
-            indicating that we've found a new minimum element within the portion of the array being searched.*/
+            /*If we find a smaller element at index i, we update minIndex to be i,
+            indicating that we've found a new minimum element within the portion
+            of the array being searched.*/
             minIndex = i;
         }
     }
@@ -131,10 +137,10 @@ const people = [
 // ===============================   Printing to the Console   =============================//
 
 // Sort numbers in ascending order
-/*This function compares two numbers a and b by subtracting b from a. 
-If the result is negative, it means a is less than b, and if it's positive, 
-it means a is greater. The selectionSort function uses this comparison function to sort 
-the numbers array in ascending order. After sorting, the sorted numbers array is logged to 
+/*This function compares two numbers a and b by subtracting b from a.
+If the result is negative, it means a is less than b, and if it's positive,
+it means a is greater. The selectionSort function uses this comparison function to sort
+the numbers array in ascending order. After sorting, the sorted numbers array is logged to
 the console.*/
 selectionSort(integers, function(a, b) {
     return a - b;
@@ -142,9 +148,9 @@ selectionSort(integers, function(a, b) {
 console.log("Sorted numbers:", numbers);
 
 // Sort strings in alphabetical order
-/*This function uses the localeCompare method to compare two strings a and b. 
-The method returns a negative value if a comes before b in lexicographic order, 
-a positive value if a comes after b, and 0 if they are equal. The selectionSort 
+/*This function uses the localeCompare method to compare two strings a and b.
+The method returns a negative value if a comes before b in lexicographic order,
+a positive value if a comes after b, and 0 if they are equal. The selectionSort
 function uses this comparison function to sort the strings array in alphabetical order.*/
 selectionSort(strings, function(a, b) {
     return a.localeCompare(b);
@@ -152,9 +158,9 @@ selectionSort(strings, function(a, b) {
 console.log("Sorted strings:", strings);
 
 // Sort people by last name and then by first name
-/*This function first checks if the last names (a.last and b.last) are equal. 
-If they are, it uses localeCompare to compare the first names (a.first and b.first). 
-This ensures that the people are sorted primarily by last name and then by first name. 
+/*This function first checks if the last names (a.last and b.last) are equal.
+If they are, it uses localeCompare to compare the first names (a.first and b.first).
+This ensures that the people are sorted primarily by last name and then by first name.
 After sorting, the sorted people array is logged to the console.*/
 
 // Custom Comparator for Sorting People by Last Name, Breaking Ties with First Name
@@ -194,29 +200,23 @@ selectionSort(people, sortByFirstNameAndLastName);
 console.log("Sorted by First Name and Last Name:");
 console.log(people);
 
-
-
-
-
-
-
 //======================================    Questions   ========================================//
 /*
-1- What happens in each case when testing the sorting function 
+1- What happens in each case when testing the sorting function
 with different data types, including strings with mixed capital and lowercase letters?
 
-When we sort arrays of integers and floating-point numbers, the sorting function will 
+When we sort arrays of integers and floating-point numbers, the sorting function will
 arrange them in ascending order.
-When sorting an array of strings, the function will arrange them in lexicographical order, 
+When sorting an array of strings, the function will arrange them in lexicographical order,
 considering capitalization for example, capital letters come before lowercase letters.
-If we sort an array with a mix of data types (integers, strings, floats), it will still work, 
-but the order might not make much sense, as the function isn't designed to handle such mixed 
+If we sort an array with a mix of data types (integers, strings, floats), it will still work,
+but the order might not make much sense, as the function isn't designed to handle such mixed
 data effectively.
 
 
 
-2- What happens when you change the comparison in your "compareTo" function from "<" to ">" in the 
+2- What happens when you change the comparison in your "compareTo" function from "<" to ">" in the
 comparator function?
 
-If you change the comparison from "<" to ">", it will reverse the order of sorting. 
+If you change the comparison from "<" to ">", it will reverse the order of sorting.
 The sorting function will sort the elements in descending order instead of ascending order.*/
