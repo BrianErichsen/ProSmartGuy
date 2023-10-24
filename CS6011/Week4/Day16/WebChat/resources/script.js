a4476b0149325f35efcc51f9815416923f93933e
Brian Erichsen Fagundes <brianerichsen@gmail.com>
2023-10-23 20:49:54 -0600

    Now chat works but still needs to refresh main msgs

--- CS6011/Week4/Day16/WebChat/resources/script.js
+++ CS6011/Week4/Day16/WebChat/resources/script.js
@@ -1,25 +1,60 @@
 const socket = new WebSocket('ws://' + location.host);
 
+//Tracks whether the use is in a room
+let inRoom = false;
+
 //Display messages in the chat window
 function displayMessage(message) {
     const chatMessages = document.getElementById('chat-messages');
     const messageDiv = document.createElement('div');
-    messageDiv.textContent = '${message.user} ${message.message}';
+    messageDiv.textContent = `${message.user} ${message.message}`;
     chatMessages.appendChild(messageDiv);
-}
+} //'`
 //Sends message to server
 function sendMessage() {
+    
+    if (inRoom) {
     const messageInput = document.getElementById('message-input');
     const message = messageInput.value;
 
-    socket.send('message ${message}');
+    socket.send(`message ${message}`);
     messageInput.value = '';
+    } else {
+        alert("You are not in a room. Please join a room before sending messages.");
+    }
 }
 //Function to join a room
-function JoinRoom() {
-    const roomName = document.getElementById('room-name').value;
-    socket.send('join <username> ${roomName}');
+function joinRoom() {
+    const roomNameInput = document.getElementById('room-name');
+    const roomName = roomNameInput.value;
+
+    //Gets the username from the input
+    const usernameInput = document.getElementById('username');
+    const username = usernameInput.value;
+
+    //if value is different than 0; then it means that it is true
+    if (roomName && username) {
+    socket.send(`join ${username} ${roomName}`);
+    //Sets user to be in a room to be true
+    inRoom = true;
+    //Update the room name display in the span id
+    document.getElementById('room-name-display').textContent = `room: ${roomName}`;
+    document.getElementById('user-name-display').textContent = `user: ${username}`;
+    } else {
+        alert("Get in it a room now!");
+    }
 }
+//Function to leave current room
+function leaveRoom() {
+    if (inRoom) {
+        inRoom = false;
+        document.getElementById('room-name-display').textContent = 'Room: None';
+        socket.send('leave');
+    } else {
+        alert("You are not in a room.");
+    }
+}
+
 //Handles incoming webSocket messages
 socket.onmessage = (event) => {
     const message = JSON.parse(event.data);
@@ -35,17 +70,7 @@ const sendButton = document.getElementById('send-button');
 sendButton.addEventListener('click', sendMessage);
 
 const joinButton = document.getElementById('join-button');
-joinButton.addEventListener('click', () => {
-    const usernameInput = docume.getElementById('username');
-    const RoomNameInput = document.getElementById('roomname');
-    const username = usernameInput.value;
-    const roomName = roomNameInput.value;
-
-    socket.send('join ${username} ${roomName}');
-
-    document.getElementById('user-name-display').textContent = 'User: ${username}';
-    document.getElementById('room-name-display').textContent = 'Room: ${roomName}';
-});
+joinButton.addEventListener('click', joinRoom);
 
 const leaveButton = document.getElementById('leave-button');
 leaveButton.addEventListener('click', leaveRoom);
