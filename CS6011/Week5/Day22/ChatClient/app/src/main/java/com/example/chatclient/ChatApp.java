ff46a78554d0d9b54d342e11877d0119cfc029e8
Brian Erichsen Fagundes <brianerichsen@gmail.com>
2023-11-01 14:38:04 -0600

    .

---
+++ CS6011/Week5/Day22/ChatClient/app/src/main/java/com/example/chatclient/ChatApp.java
@@ -0,0 +1,14 @@
+package com.example.chatclient;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+
+public class ChatApp extends Fragment {
+//    @Override
+//    public View onCreateView (
+//            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState
+//    )
+}//End of class bracket
--- CS6011/Week5/Day22/ChatClient/app/src/main/java/com/example/chatclient/MainActivity.java
+++ CS6011/Week5/Day22/ChatClient/app/src/main/java/com/example/chatclient/MainActivity.java
@@ -2,16 +2,18 @@ package com.example.chatclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    Boolean isLoginSuccessful = true;
+    Boolean isLoginSuccessful = false;
     EditText username;
     EditText password;
     Button login;
@@ -26,41 +28,48 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.activity_main);
         //Loads Username and Password fields to be edited
         setContentView(R.layout.login);
+
         login = (Button) findViewById(R.id.Login);
         username = (EditText) findViewById((R.id.usernameEditText));
         password = (EditText) findViewById((R.id.passwordEditText));
 
         exit = (Button) findViewById(R.id.Exit);
         tx1 = (TextView) findViewById(R.id.textView3);
+        tx1.setVisibility(View.GONE);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("admin") && password.getText()
+                        .toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Redirecting ...", Toast.LENGTH_LONG).show();
+                    isLoginSuccessful = true;
+                    setContentView(R.layout.chat);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_LONG).show();
+
+                    tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        login.setEnabled(false);
+                    }
+                }
+            }
+        });
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //WebSocket ws = new WebSocketAdpater();
+        //ws.connect(); initiates the handshake
 
 
-    }
-    // --- prob make a class called user;
+    }//End of onCreate method bracket
 
-    public void onLoginClick(View view) {
-        //Gets the username and password
-        EditText username = (EditText) findViewById(R.id.usernameEditText);
-        EditText password = (EditText) findViewById(R.id.passwordEditText);
-
-        String usernameStr = username.getText().toString();
-        String passwordStr = password.getText().toString();
-
-        if (isLoginSuccessful) {
-            setContentView(R.layout.chat);
-        }
-    }
-    public void login (View view) {
-        //Gets the username and password
-        EditText username = (EditText) findViewById(R.id.usernameEditText);
-        EditText password = (EditText) findViewById(R.id.passwordEditText);
-
-        String usernameStr = username.getText().toString();
-        String passwordStr = password.getText().toString();
-        if(usernameStr.equals("admin") && passwordStr.equals("admin")) {
-            //correct password
-            isLoginSuccessful = true;
-        } else {
-
-        }
-}
 }//End of MainActivity bracket
